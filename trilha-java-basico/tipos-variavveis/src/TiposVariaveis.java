d6e4375eadb275eb693e915d616ff6f6ff2083f8
Carlos-Tucche <carlos.dias2021@gmail.com>
2024-08-27 18:13:52 -0300

    tipos variaves e operadores

--- trilha-java-basico/tipos-variavveis/src/TiposVariaveis.java
+++ trilha-java-basico/tipos-variavveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
 public class  TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+     boolean condicao1 = true;
+     boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("condicao1 e a condicao2 são verdadeiras?.");
+            
+        }
+    System.out.println("fim");
+
+        if (condicao1 || condicao2){
+            System.out.println("Condicao1 ou condicao2, uma ou outra e vedadeira?");
+        }
+
     }
 }
