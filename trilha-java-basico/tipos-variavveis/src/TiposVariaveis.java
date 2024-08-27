e3c45a7028570e25e919ac2af47eda38b1b8e6be
Carlos-Tucche <carlos.dias2021@gmail.com>
2024-08-27 20:06:37 -0300

    ciração do projeto smart tv

--- trilha-java-basico/tipos-variavveis/src/TiposVariaveis.java
+++ trilha-java-basico/tipos-variavveis/src/TiposVariaveis.java
@@ -1,18 +1,5 @@
 public class  TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         
-     boolean condicao1 = true;
-     boolean condicao2 = false;
-
-        if(condicao1 && condicao2){
-            System.out.println("condicao1 e a condicao2 são verdadeiras?.");
-            
-        }
-    System.out.println("fim");
-
-        if (condicao1 || condicao2){
-            System.out.println("Condicao1 ou condicao2, uma ou outra e vedadeira?");
-        }
-
     }
 }
